ae0bca763d4480f8eb7a8dac5e6961157038baf4
Ansh Ajay Agarwal <130859572+ansh1593@users.noreply.github.com>
2026-02-18 19:39:14 +0530

    Delete Ansh_Agarwal/Day1/Codes directory

--- Ansh_Agarwal/Day1/Codes/TF_IDF.py
+++
@@ -1,9 +0,0 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-tfidf_vectorizer = TfidfVectorizer()
-X_tfidf = tfidf_vectorizer.fit_transform(sentences)
-
-tfidf_df = pd.DataFrame(X_tfidf.toarray(), columns=tfidf_vectorizer.get_feature_names_out())
-
-print("\nTF-IDF Matrix:")
-print(tfidf_df)
