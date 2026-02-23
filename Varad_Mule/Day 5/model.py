8cf85a7645e5c154eeafd4f789a292f2fec530bb
Varad1507 <121933462+Varad1507@users.noreply.github.com>
2026-02-23 16:49:43 +0530

    Add files via upload

---
+++ Varad_Mule/Day 5/model.py
@@ -0,0 +1,31 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+X_train = X_train / 255.0
+X_test = X_test / 255.0
+
+model = Sequential([
+    Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)),
+    MaxPooling2D((2,2)),
+    Conv2D(64, (3,3), activation='relu'),
+    MaxPooling2D((2,2)),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(X_train, y_train, epochs=5, validation_split=0.2)
+
+loss, accuracy = model.evaluate(X_test, y_test)
+print("Test Accuracy:", accuracy)
+
+model.save("cifar_model.h5")
+print("Model saved successfully!")
